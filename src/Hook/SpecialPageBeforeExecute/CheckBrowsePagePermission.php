898c70f19d8ffd9b4c0ce69ffbc8d6385aee22b9
Hamish <hamish.slater@hallowelt.com>
2026-03-30 18:41:27 +0700

    Add read permission check for Special:Browse
    ERM46929
    
    [5.1.x]
    
    Change-Id: Iac8ea87a528a7378b4c28981859b1697f53a4477
    (cherry picked from commit b305a6e1fa17bfb3f22a97879b00695594b69873)

---
+++ src/Hook/SpecialPageBeforeExecute/CheckBrowsePagePermission.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace BlueSpice\PageAccess\Hook\SpecialPageBeforeExecute;
+
+use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\SpecialPage\Hook\SpecialPageBeforeExecuteHook;
+use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleFactory;
+use PermissionsError;
+
+class CheckBrowsePagePermission implements SpecialPageBeforeExecuteHook {
+
+	/** @var TitleFactory */
+	protected $titleFactory;
+
+	/** @var PermissionManager */
+	protected $permissionManager;
+
+	/**
+	 * @param TitleFactory $titleFactory
+	 * @param PermissionManager $permissionManager
+	 */
+	public function __construct( TitleFactory $titleFactory, PermissionManager $permissionManager ) {
+		$this->titleFactory = $titleFactory;
+		$this->permissionManager = $permissionManager;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onSpecialPageBeforeExecute( $special, $subPage ) {
+		if ( $special->getName() !== 'Browse' ) {
+			return;
+		}
+
+		if ( !$subPage ) {
+			return;
+		}
+
+		$title = $this->parseTitle( $subPage );
+		if ( !$title ) {
+			return;
+		}
+
+		$userCanReadPage = $this->permissionManager->userCan(
+			'read',
+			$special->getUser(),
+			$title
+		);
+
+		if ( !$userCanReadPage ) {
+			throw new PermissionsError( 'read' );
+		}
+	}
+
+	/**
+	 * @param string $page
+	 * @return Title|null
+	 */
+	private function parseTitle( string $page ): ?Title {
+		$page = ltrim( $page, ':' );
+
+		$page = str_replace(
+			[ '-2F', '-3A' ],
+			[ '/', ':' ],
+			$page
+		);
+
+		// strip SMW subobject identifier
+		$page = strstr( $page, '-23', true ) ?: $page;
+
+		return $this->titleFactory->newFromText( $page );
+	}
+
+}
