da3f012eae28fde616f7dc94223a9d219c000b22
Robert Vogel <vogel@hallowelt.biz>
2026-04-27 15:54:15 +0200

    Use WAN cache for page_properties lookup
    ERM47582
    [ai authored]
    [5.1.x]
    
    Change-Id: I3c71a73eb2a53d83aa93fd822b0888c34a717f5e

--- src/CheckAccess.php
+++ src/CheckAccess.php
@@ -6,8 +6,13 @@ use MediaWiki\Config\Config;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
 use MediaWiki\User\User;
+use Wikimedia\ObjectCache\WANObjectCache;
+use Wikimedia\Rdbms\IConnectionProvider;
 
 class CheckAccess {
+
+	public const CACHE_KEY = 'BlueSpicePageAccess-AllAccessGroups';
+	private const CACHE_TTL = WANObjectCache::TTL_HOUR;
 
 	/**
 	 * @var Config
@@ -15,15 +20,39 @@ class CheckAccess {
 	protected $config = null;
 
 	/**
+	 * @var WANObjectCache
+	 */
+	private $cache;
+
+	/**
+	 * @var IConnectionProvider
+	 */
+	private $connectionProvider;
+
+	/**
 	 * @var string[]
 	 */
 	protected static $accessGroupsByPageId = [];
 
 	/**
-	 * @param Config $config
+	 * In-process map of page_id => groups string from page_props, loaded once per request.
+	 * @var array|null
 	 */
-	public function __construct( Config $config ) {
+	private $allAccessGroupProps = null;
+
+	/**
+	 * @param Config $config
+	 * @param WANObjectCache $cache
+	 * @param IConnectionProvider $connectionProvider
+	 */
+	public function __construct(
+		Config $config,
+		WANObjectCache $cache,
+		IConnectionProvider $connectionProvider
+	) {
 		$this->config = $config;
+		$this->cache = $cache;
+		$this->connectionProvider = $connectionProvider;
 	}
 
 	/**
@@ -73,13 +102,14 @@ class CheckAccess {
 		if ( isset( static::$accessGroupsByPageId[ $title->getArticleID() ] ) ) {
 			return static::$accessGroupsByPageId[ $title->getArticleID() ];
 		}
+
+		$allAccessGroupProps = $this->getAllAccessGroupProps();
+
 		$allTitles = $title->getTemplateLinksFrom();
 		$allTitles[] = $title;
 		$accessGroups = [];
-		$pageProps = MediaWikiServices::getInstance()->getPageProps();
 		foreach ( $allTitles as $titleToCheck ) {
-			$titleProp = $pageProps->getProperties( $titleToCheck, 'bs-page-access' );
-			$prop = $titleProp[$titleToCheck->getArticleID()] ?? '';
+			$prop = $allAccessGroupProps[$titleToCheck->getArticleID()] ?? '';
 			if ( $prop === '' ) {
 				continue;
 			}
@@ -91,6 +121,59 @@ class CheckAccess {
 
 		static::$accessGroupsByPageId[ $title->getArticleID() ] = array_unique( $accessGroups );
 		return static::$accessGroupsByPageId[ $title->getArticleID() ];
+	}
+
+	/**
+	 * Bulk-loads all bs-page-access properties from the page_props table,
+	 * using the WAN cache to avoid repeated DB lookups.
+	 *
+	 * @return array Map of page_id => groups string
+	 */
+	private function getAllAccessGroupProps(): array {
+		if ( $this->allAccessGroupProps !== null ) {
+			return $this->allAccessGroupProps;
+		}
+
+		$cacheKey = $this->cache->makeKey( self::CACHE_KEY );
+		$this->allAccessGroupProps = $this->cache->getWithSetCallback(
+			$cacheKey,
+			self::CACHE_TTL,
+			function () {
+				return $this->fetchAllAccessGroupPropsFromDB();
+			}
+		);
+
+		return $this->allAccessGroupProps;
+	}
+
+	/**
+	 * @return array Map of page_id => groups string
+	 */
+	private function fetchAllAccessGroupPropsFromDB(): array {
+		$dbr = $this->connectionProvider->getReplicaDatabase();
+		$result = $dbr->newSelectQueryBuilder()
+			->select( [ 'pp_page', 'pp_value' ] )
+			->from( 'page_props' )
+			->where( [ 'pp_propname' => 'bs-page-access' ] )
+			->caller( __METHOD__ )
+			->fetchResultSet();
+
+		$map = [];
+		foreach ( $result as $row ) {
+			$map[(int)$row->pp_page] = $row->pp_value;
+		}
+		return $map;
+	}
+
+	/**
+	 * Invalidate the WAN cache for all access group properties.
+	 * Should be called when page access settings change.
+	 */
+	public function invalidateCache(): void {
+		$cacheKey = $this->cache->makeKey( self::CACHE_KEY );
+		$this->cache->delete( $cacheKey );
+		$this->allAccessGroupProps = null;
+		static::$accessGroupsByPageId = [];
 	}
 
 	/**
